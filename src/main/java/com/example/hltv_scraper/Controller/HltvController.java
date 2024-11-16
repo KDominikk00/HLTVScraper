cb6bf5fa4c01bb181020e003b6a535004fe68b3b
Dominik Kowalski <dominikkowalski331@gmail.com>
2024-11-16 14:28:25 +0100

    Add CORS config

--- src/main/java/com/example/hltv_scraper/Controller/HltvController.java
+++ src/main/java/com/example/hltv_scraper/Controller/HltvController.java
@@ -5,6 +5,7 @@ import com.example.hltv_scraper.Service.HltvScraperService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController
@@ -20,10 +21,10 @@ public class HltvController {
     @GetMapping("/players")
     public List<PlayerDTO> getPlayers() {
         try {
-            return scraperService.scrapePlayersWithSelenium();
+            return scraperService.getPlayers(); // Use the caching logic
         } catch (InterruptedException e) {
             e.printStackTrace();
-            return List.of();  // Return an empty list or handle errors as needed
+            return List.of(); // Return an empty list or handle errors as needed
         }
     }
 }
